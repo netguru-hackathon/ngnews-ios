d9977cbaa8db30385be0bc8a790246d09ebfe82f
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2015-08-21 18:10:32 +0200

    Add news list element component

---
+++ components/NewsListElement.js
@@ -0,0 +1,58 @@
+var React = require('react-native');
+var {
+  Image,
+  ListView,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View
+} = React;
+
+class NewsListElement extends React.Component {
+
+  render() {
+    return (
+      <TouchableHighlight onPress={this.props.onPress}>
+        <View style={styles.rowView}>
+          <Image style={styles.thumbImage} source={{uri: this.props.icon}} />
+          <View style={styles.contentView}>
+            <Text style={styles.timeText}>{this.props.time}</Text>
+            <Text style={styles.titleText}>{this.props.title}</Text>
+          </View>
+        </View>
+      </TouchableHighlight>
+    );
+  }
+
+}
+
+var styles = StyleSheet.create({
+  rowView: {
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+  },
+  thumbImage: {
+    alignSelf: 'center',
+    width: 48,
+    height: 48,
+  },
+  contentView: {
+    flex: 1,
+    flexWrap: 'wrap',
+    flexDirection: 'column',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    marginLeft: 16,
+  },
+  timeText: {
+    color: '#ccc',
+    fontSize: 12,
+  },
+  titleText: {
+    fontSize: 14,
+    marginTop: 8,
+  },
+});
+
+module.exports = NewsListElement;
